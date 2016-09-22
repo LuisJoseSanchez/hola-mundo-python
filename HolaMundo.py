b946e62123eb3701212c2e27d7060ba9b0f3b533
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-09-22 09:07:10 +0200

    Saludo personalizado

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,4 +1,6 @@
 # Hola mundo en Python
 
 print "Hola mundo!"
-print "Me esta gustando Python"
+print "Me esta gustando Python" 
+nombre = raw_input("Dime como te llamas: ")
+print "Encantado de saludarte ", nombre
