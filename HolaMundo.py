ec0da0b23cc2f324f62ca7f9ced6249a615d83da
rodcito <rodmycespedesarze@gmail.com>
2022-04-11 01:09:24 -0400

    Cambio de color al boton

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,6 +1,6 @@
 # Hola mundo en Python
 
-print "Hola mundo!"
+print "Hola mundo de programadores!"
 print "Me esta gustando Python" 
 nombre = raw_input("Dime como te llamas: ")
 print "Encantado de saludarte ", nombre
